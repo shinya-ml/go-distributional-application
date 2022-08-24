b89aa20aa4c09c337112994436bb831689d1490d
shinya-ml <shinya_suzuki@voyagegroup.com>
2022-08-24 10:29:43 +0900

    close

--- internal/log/log.go
+++ internal/log/log.go
@@ -110,3 +110,18 @@ func (l *Log) Read(off uint64) (*api.Record, error) {
 	}
 	return s.Read(off)
 }
+
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) Remove() error {
+	return nil
+}
